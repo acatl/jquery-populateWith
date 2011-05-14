b794c59b985661be9ae3f5f00051d44218702e69
acatl pacheco <acatl.pacheco@gmail.com>
2011-05-14 01:10:39 -0500

    changed name of plugin to populateWith and added readme instructions

---
+++ jquery.populateWith.js
@@ -0,0 +1,59 @@
+/*
+ * populateWith jquery plugin
+ *
+ * Copyright 2011, Acatl Pacheco
+ * Licensed under the MIT License.
+ *
+ */
+
+(function($){
+	
+  $.fn.populateWith = function(data, options){
+		var defaultOptions = {
+			empty: true, 
+			valueField: null, 
+			valueFunction: null,
+			textField:null,
+			textFunction:null
+		};
+		
+		var optionsSelected = $.extend({}, defaultOptions, options);
+		
+		var optionsOutput = "";
+		for(var index in data) {
+			var element = data[index];
+			
+			
+			
+			var value = index;
+			if ( optionsSelected.valueFunction!= null) {
+				value = optionsSelected.valueFunction(element, index);
+			} else if (	optionsSelected.valueField!= null) {
+				value = element[optionsSelected.valueField];
+			}
+			
+			if ( optionsSelected.textFunction!= null) {
+				text = optionsSelected.textFunction(element, index);
+			} else if (	optionsSelected.textField!= null) {
+				text = element[optionsSelected.textField];
+			} else if(element.label != null) {
+				text = element.label;
+			} else if(typeof element == 'string' || typeof element == 'number') {
+				text = element;
+			}
+			
+			optionsOutput+='<option value="' + value + '">' + text + '</option>';
+		}
+	
+		return this.each(function(){
+			if (optionsSelected.empty) {
+				$(this).children().remove();
+				$(this).html(optionsOutput);
+			} else {
+				$(this).append(optionsOutput);
+			}
+      
+    });
+    
+  };
+})(jQuery);
--- test/test.populateWith.js
+++ test/test.populateWith.js
@@ -2,8 +2,8 @@ $(function(){
 	
 	
 	
-	/* UICOMPONENT WIDGET */
-	module("jquery populate plugin", {
+	/* populateWith plugin */
+	module("jquery populateWith plugin", {
 		setup : function(){
 			this.dataSourceSimple = [
 				"option 1",
@@ -27,68 +27,68 @@ $(function(){
 	
     
 	test("should accept simple data source", function(){
-		this.select.populate(this.dataSourceSimple);	
+		this.select.populateWith(this.dataSourceSimple);	
 		console.log(this.select);
 		equals(this.select.children().length, 4, "options created");
 	});
 
 	test("should accept complex data source", function(){
-		this.select.populate(this.dataSourceComplex);	
+		this.select.populateWith(this.dataSourceComplex);	
 		console.log(this.select);
 		equals(this.select.children().length, 4, "options created");
 	});
 
 	test("should empty children elements before adding new", function(){
-		this.select.populate([], {empty:true});	
+		this.select.populateWith([], {empty:true});	
 		console.log(this.select);
 		equals(this.select.children().length, 0, "options removed");
 	});
 	
 	test("should not empty children elements before adding new when empty=false", function(){
-		this.select.populate(this.dataSourceSimple);	
-		this.select.populate([], {empty:false});	
+		this.select.populateWith(this.dataSourceSimple);	
+		this.select.populateWith([], {empty:false});	
 		console.log(this.select);
-		equals(this.select.children().length, 4, "options removed");
+		equals(this.select.children().length, 4, "options where appended");
 	});
 	
 	test("value should be calculated by index by default", function(){
-		this.select.populate(this.dataSourceSimple);	
+		this.select.populateWith(this.dataSourceSimple);	
 		console.log(this.select);
-		equals(this.select.children().first().attr("value"), 0, "options removed");
+		equals(this.select.children().first().attr("value"), 0, "options have default index");
 	});
 
 	test("value should be calculated by valueField when provided", function(){
-		this.select.populate(this.dataSourceComplex, {valueField:"id"});	
+		this.select.populateWith(this.dataSourceComplex, {valueField:"id"});	
 		console.log(this.select);
-		equals(this.select.children().first().attr("value"), "2345", "options removed");
+		equals(this.select.children().first().attr("value"), "2345", "options have custom value calculated by valueField");
 	});
 	
 	test("value should be calculated by valueFunction when provided", function(){
-		this.select.populate(this.dataSourceComplex, {valueFunction:function(element, index){
+		this.select.populateWith(this.dataSourceComplex, {valueFunction:function(element, index){
 			return index + "_" + element.id;
 		}});	
 		console.log(this.select);
-		equals(this.select.children().first().attr("value"), "0_2345", "options removed");
+		equals(this.select.children().first().attr("value"), "0_2345", "options have custom value calculated by valueFunction");
 	});
 	
 	test("text should be set by element by default", function(){
-		this.select.populate(this.dataSourceSimple);	
+		this.select.populateWith(this.dataSourceSimple);	
 		console.log(this.select);
-		equals(this.select.children().first().text(), "option 1", "options removed");
+		equals(this.select.children().first().text(), "option 1", "options have default text");
 	});
 
 	test("text should be calculated by textField when provided", function(){
-		this.select.populate(this.dataSourceComplex, {textField:"label"});	
+		this.select.populateWith(this.dataSourceComplex, {textField:"label"});	
 		console.log(this.select);
-		equals(this.select.children().first().text(), "option 1", "options removed");
+		equals(this.select.children().first().text(), "option 1", "options have custom text calculated by textField");
 	});
 	
 	test("text should be calculated by textFunction when provided", function(){
-		this.select.populate(this.dataSourceComplex, {textFunction:function(element, index){
+		this.select.populateWith(this.dataSourceComplex, {textFunction:function(element, index){
 			return index + "_" + element.label;
 		}});	
 		console.log(this.select);
-		equals(this.select.children().first().text(), "0_option 1", "options removed");
+		equals(this.select.children().first().text(), "0_option 1", "options have custom text calculated by textFunction");
 	});
 	
 });
